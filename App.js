3e510fba625811ad3c385544f1399278547b6a83
Debakor10 <debakor10@gmail.com>
2021-06-04 02:35:57 +0600

    banner complete

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
-import Footer from './app/components/Footer';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { View, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import NavBar from './app/components/NavBar';
 
+
 export default function App () {
   return (
-    <View >
+    
+     <>
       <NavigationContainer>
         <NavBar></NavBar>
       </NavigationContainer>
-      <Footer></Footer>
-    </View>
+     
+    </>
   )
 }
